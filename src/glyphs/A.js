7134cd99ce894f00ba413447995e274f12b7bc47
multipleof4 <planetrenox@protonmail.com>
2025-11-09 09:10:25 -0800

    Refactor: Professional Candara-inspired A/a

--- src/glyphs/A.js
+++ src/glyphs/A.js
@@ -3,9 +3,7 @@ const makeGlyph = (opentype, name, unicode, cmds, adv) =>
     name,
     unicode,
     advanceWidth: adv,
-    path: cmds(
-      new opentype.Path()
-    )
+    path: cmds(new opentype.Path())
   });
 
 export const glyphA = (opentype, adv) =>
@@ -14,19 +12,18 @@ export const glyphA = (opentype, adv) =>
     "A",
     "A".codePointAt(0),
     (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(260, 800);
-      p.lineTo(340, 800);
-      p.lineTo(500, 0);
-      p.lineTo(420, 0);
-      p.lineTo(375, 200);
-      p.lineTo(225, 200);
-      p.lineTo(180, 0);
+      p.moveTo(50, 0);
+      p.lineTo(280, 700);
+      p.lineTo(320, 700);
+      p.lineTo(550, 0);
+      p.lineTo(480, 0);
+      p.lineTo(430, 180);
+      p.lineTo(170, 180);
+      p.lineTo(120, 0);
       p.close();
-      p.moveTo(250, 300);
-      p.lineTo(350, 300);
-      p.lineTo(330, 420);
-      p.lineTo(270, 420);
+      p.moveTo(195, 240);
+      p.lineTo(405, 240);
+      p.lineTo(300, 620);
       p.close();
       return p;
     },
@@ -39,26 +36,26 @@ export const glypha = (opentype, adv) =>
     "a",
     "a".codePointAt(0),
     (p) => {
-      p.moveTo(130, 200);
-      p.curveTo(130, 100, 210, 40, 320, 40);
-      p.curveTo(410, 40, 470, 80, 500, 140);
-      p.lineTo(500, 60);
-      p.lineTo(560, 60);
-      p.lineTo(560, 380);
-      p.curveTo(560, 520, 460, 600, 320, 600);
-      p.curveTo(210, 600, 130, 540, 120, 450);
-      p.lineTo(200, 450);
-      p.curveTo(210, 510, 250, 540, 320, 540);
-      p.curveTo(410, 540, 480, 490, 480, 390);
-      p.lineTo(480, 340);
-      p.curveTo(450, 390, 390, 420, 320, 420);
-      p.curveTo(210, 420, 130, 340, 130, 200);
+      p.moveTo(340, -10);
+      p.curveTo(240, -10, 160, 30, 120, 100);
+      p.lineTo(170, 130);
+      p.curveTo(200, 80, 260, 50, 330, 50);
+      p.curveTo(420, 50, 480, 100, 480, 180);
+      p.lineTo(480, 220);
+      p.curveTo(450, 180, 400, 150, 330, 150);
+      p.curveTo(220, 150, 140, 220, 140, 320);
+      p.curveTo(140, 420, 220, 490, 330, 490);
+      p.curveTo(400, 490, 450, 460, 480, 420);
+      p.lineTo(480, 480);
+      p.lineTo(540, 480);
+      p.lineTo(540, 180);
+      p.curveTo(540, 70, 460, -10, 340, -10);
       p.close();
-      p.moveTo(320, 100);
-      p.curveTo(240, 100, 190, 150, 190, 210);
-      p.curveTo(190, 290, 240, 340, 320, 340);
-      p.curveTo(400, 340, 450, 290, 450, 210);
-      p.curveTo(450, 150, 400, 100, 320, 100);
+      p.moveTo(330, 210);
+      p.curveTo(410, 210, 480, 250, 480, 320);
+      p.curveTo(480, 390, 410, 430, 330, 430);
+      p.curveTo(250, 430, 200, 380, 200, 320);
+      p.curveTo(200, 260, 250, 210, 330, 210);
       p.close();
       return p;
     },
