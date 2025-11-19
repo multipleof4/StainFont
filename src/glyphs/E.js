ba600a5cce6461d97e8eb6e417d19158009652c9
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:32:22 -0800

    Fix: Refine lowercase e eye and terminal

--- src/glyphs/E.js
+++ src/glyphs/E.js
@@ -1,9 +1,11 @@
 export const glyphE=(o,w)=>new o.Glyph({name:"E",unicode:69,advanceWidth:w,path:(p=>{
   p.moveTo(90,0).lineTo(90,700).lineTo(530,700).lineTo(530,625).lineTo(175,625).lineTo(175,390).lineTo(480,390).lineTo(480,315).lineTo(175,315).lineTo(175,75).lineTo(540,75).lineTo(540,0).close(); return p;
 })(new o.Path())});
+
 export const glyphe=(o,w)=>new o.Glyph({name:"e",unicode:101,advanceWidth:w,path:(p=>{
-  p.moveTo(475,225).curveTo(475,150,435,60,370,60).curveTo(290,60,255,100,255,150).close() // trick
-   .moveTo(470,265).lineTo(130,265).curveTo(133,180,160,-15,310,-15).curveTo(390,-15,450,20,475,90).lineTo(515,65)
-   .curveTo(480,-15,415,-80,280,-65).curveTo(145,-50,55,70,55,225).curveTo(55,385,130,506,260,506).curveTo(425,506,475,380,470,265).close()
-   .moveTo(135,305).curveTo(140,375,190,440,260,440).curveTo(355,440,400,355,400,305).close(); return p;
+  p.moveTo(470,270).curveTo(470,420,380,510,265,510).curveTo(130,510,45,390,45,250).curveTo(45,100,135,-15,270,-15)
+   .curveTo(380,-15,450,40,480,130).lineTo(410,150).curveTo(390,90,340,50,270,50).curveTo(160,50,120,130,115,220)
+   .lineTo(470,220).lineTo(470,270).close()
+   .moveTo(115,270).curveTo(120,370,170,450,265,450).curveTo(360,450,400,380,400,270).lineTo(115,270).close();
+  return p;
 })(new o.Path())});
