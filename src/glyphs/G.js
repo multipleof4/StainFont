96319ae100ea0c24c38b02657b32458ef305e83c
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:32:24 -0800

    Fix: Refine G spur and g double-storey link

--- src/glyphs/G.js
+++ src/glyphs/G.js
@@ -1,12 +1,16 @@
 export const glyphG=(o,w)=>new o.Glyph({name:"G",unicode:71,advanceWidth:w,path:(p=>{
-  p.moveTo(590,440).lineTo(590,240).lineTo(350,240).lineTo(350,315).lineTo(510,315).lineTo(510,345)
-   .curveTo(490,530,425,645,320,645).curveTo(205,645,145,500,145,350).curveTo(145,185,215,65,330,65).curveTo(420,65,480,105,510,185)
-   .lineTo(575,160).curveTo(535,45,440,-15,330,-15).curveTo(150,-15,60,165,60,350).curveTo(60,560,165,715,320,715).curveTo(440,715,550,610,590,440).close(); return p;
+  p.moveTo(580,290).lineTo(330,290).lineTo(330,360).lineTo(500,360).lineTo(500,390).curveTo(480,560,410,655,320,655)
+   .curveTo(190,655,135,520,135,360).curveTo(135,200,190,65,320,65).curveTo(410,65,470,110,500,190).lineTo(570,160)
+   .curveTo(530,50,440,-15,320,-15).curveTo(140,-15,50,160,50,360).curveTo(50,560,150,725,320,725).curveTo(450,725,550,610,580,390).close(); return p;
 })(new o.Path())});
+
 export const glyphg=(o,w)=>new o.Glyph({name:"g",unicode:103,advanceWidth:w,path:(p=>{
-  p.moveTo(475,230).lineTo(475,-45).curveTo(475,-185,405,-215,325,-215).lineTo(165,-215).lineTo(165,-150).lineTo(295,-150)
-  .curveTo(380,-150,395,-110,395,-40).lineTo(395,45).curveTo(355,0,300,-15,245,-15).curveTo(120,-15,45,85,45,250)
-  .curveTo(45,410,120,506,240,506).lineTo(485,506).lineTo(485,435).lineTo(280,435).curveTo(215,435,130,400,130,260)
-  .curveTo(130,135,185,65,265,65).curveTo(340,65,395,100,395,200).curveTo(395,290,350,320,280,335).lineTo(475,345).close()
-  .moveTo(415,395).curveTo(415,375,350,375,320,375).curveTo(365,395,400,415,415,445).close(); return p;
+  p.moveTo(440,250).curveTo(440,300,420,340,380,370).lineTo(470,380).lineTo(470,500).lineTo(190,500).curveTo(90,500,50,430,50,360)
+   .curveTo(50,290,100,240,170,230).curveTo(110,210,70,160,70,100).curveTo(70,30,130,-15,210,-15).lineTo(330,-15)
+   .curveTo(410,-15,460,-50,460,-120).curveTo(460,-180,410,-215,310,-215).curveTo(210,-215,160,-180,150,-130).lineTo(80,-140)
+   .curveTo(100,-220,180,-275,310,-275).curveTo(470,-275,540,-200,540,-110).curveTo(540,-20,470,20,390,35)
+   .curveTo(420,50,440,80,440,120).lineTo(440,250).close()
+   .moveTo(360,100).curveTo(360,50,310,40,250,40).curveTo(180,40,140,60,140,110).curveTo(140,160,180,180,250,180).curveTo(320,180,360,150,360,100).close()
+   .moveTo(390,360).curveTo(390,310,350,290,270,290).curveTo(190,290,130,320,130,380).curveTo(130,440,180,450,270,450).curveTo(360,450,390,420,390,360).close();
+  return p;
 })(new o.Path())});
