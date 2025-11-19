cbbf086dfac7c0749bfa722b9530110d01ce8fdd
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:16:59 -0800

    Refactor: Asymmetric B/b

--- src/glyphs/B.js
+++ src/glyphs/B.js
@@ -1,14 +1,11 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphB=(o,w)=>mk(o,"B",66,p=>{
-  p.moveTo(80,0);p.lineTo(80,700);p.lineTo(340,700);p.curveTo(520,700,520,380,340,380);
-  p.lineTo(160,380);p.lineTo(160,320);p.lineTo(360,320);p.curveTo(560,320,560,0,360,0);p.close();
-  p.moveTo(160,440);p.lineTo(340,440);p.curveTo(440,440,440,640,340,640);p.lineTo(160,640);p.close();
-  p.moveTo(160,60);p.lineTo(360,60);p.curveTo(480,60,480,260,360,260);p.lineTo(160,260);p.close();
-  return p;
-},w);
-export const glyphb=(o,w)=>mk(o,"b",98,p=>{
-  p.moveTo(80,0);p.lineTo(80,710);p.lineTo(160,710);p.lineTo(160,480);
-  p.lineTo(320,480);p.curveTo(540,480,540,0,320,0);p.close();
-  p.moveTo(160,60);p.lineTo(320,60);p.curveTo(460,60,460,420,320,420);p.lineTo(160,420);p.close();
-  return p;
-},w);
+export const glyphB=(o,w)=>new o.Glyph({name:"B",unicode:66,advanceWidth:w,path:(p=>{
+  p.moveTo(90,0).lineTo(90,700).lineTo(330,700).curveTo(540,700,580,560,580,500).curveTo(580,410,520,375,440,370)
+  .curveTo(540,355,600,290,600,190).curveTo(600,60,480,0,310,0).close()
+  .moveTo(180,395).lineTo(310,395).curveTo(410,395,490,425,490,515).curveTo(490,615,400,625,310,625).lineTo(180,625).close()
+  .moveTo(180,75).lineTo(320,75).curveTo(440,75,510,90,510,215).curveTo(510,305,420,320,320,320).lineTo(180,320).close(); return p;
+})(new o.Path())});
+export const glyphb=(o,w)=>new o.Glyph({name:"b",unicode:98,advanceWidth:w,path:(p=>{
+  p.moveTo(90,720).lineTo(170,720).lineTo(170,560).curveTo(210,660,310,680,420,590).curveTo(515,520,525,370,515,270)
+  .curveTo(505,100,415,-15,285,-15).curveTo(200,-15,170,30,170,60).lineTo(170,0).lineTo(90,0).close()
+  .moveTo(170,260).curveTo(170,150,220,60,310,60).curveTo(400,60,435,160,435,280).curveTo(435,420,385,510,300,510).curveTo(220,510,170,420,170,320).close(); return p;
+})(new o.Path())});
