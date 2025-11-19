dfe0f0c3855299172d965208dfebbd59727488d3
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:13 -0800

    Refactor: Crisp I/i

--- src/glyphs/I.js
+++ src/glyphs/I.js
@@ -1,10 +1,6 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphI=(o,w)=>mk(o,"I",73,p=>{
-  p.moveTo(260,0);p.lineTo(260,700);p.lineTo(340,700);p.lineTo(340,0);p.close();
-  return p;
-},w);
-export const glyphi=(o,w)=>mk(o,"i",105,p=>{
-  p.moveTo(260,0);p.lineTo(260,480);p.lineTo(340,480);p.lineTo(340,0);p.close();
-  p.moveTo(260,600);p.lineTo(260,680);p.lineTo(340,680);p.lineTo(340,600);p.close();
-  return p;
-},w);
+export const glyphI=(o,w)=>new o.Glyph({name:"I",unicode:73,advanceWidth:w,path:(p=>{
+  p.moveTo(90,0).lineTo(90,700).lineTo(175,700).lineTo(175,0).close(); return p;
+})(new o.Path())});
+export const glyphi=(o,w)=>new o.Glyph({name:"i",unicode:105,advanceWidth:w,path:(p=>{
+  p.moveTo(85,0).lineTo(85,500).lineTo(165,500).lineTo(165,0).close().moveTo(80,590).lineTo(80,690).lineTo(170,690).lineTo(170,590).close(); return p;
+})(new o.Path())});
