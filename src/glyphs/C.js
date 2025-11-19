32379bc5b63d19ecf49cc44fc2faa28e82d2a336
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:00 -0800

    Refactor: Open aperture C/c

--- src/glyphs/C.js
+++ src/glyphs/C.js
@@ -1,13 +1,10 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphC=(o,w)=>mk(o,"C",67,p=>{
-  p.moveTo(540,200);p.lineTo(460,180);p.curveTo(440,80,300,70,300,70);p.curveTo(160,70,160,630,300,630);
-  p.curveTo(440,630,460,520,460,520);p.lineTo(540,500);p.curveTo(520,710,300,710,300,710);
-  p.curveTo(60,710,60,-10,300,-10);p.curveTo(520,-10,540,200,540,200);p.close();
-  return p;
-},w);
-export const glyphc=(o,w)=>mk(o,"c",99,p=>{
-  p.moveTo(480,140);p.lineTo(400,120);p.curveTo(380,60,300,60,300,60);p.curveTo(160,60,160,420,300,420);
-  p.curveTo(380,420,400,360,400,360);p.lineTo(480,340);p.curveTo(460,490,300,490,300,490);
-  p.curveTo(80,490,80,-10,300,-10);p.curveTo(460,-10,480,140,480,140);p.close();
-  return p;
-},w);
+export const glyphC=(o,w)=>new o.Glyph({name:"C",unicode:67,advanceWidth:w,path:(p=>{
+  p.moveTo(560,190).curveTo(520,60,440,-15,320,-15).curveTo(140,-15,60,160,60,350).curveTo(60,550,160,715,330,715)
+   .curveTo(445,715,530,650,565,530).lineTo(490,510).curveTo(460,590,410,640,330,640).curveTo(200,640,145,510,145,350)
+   .curveTo(145,200,205,65,320,65).curveTo(395,65,455,100,480,175).close(); return p;
+})(new o.Path())});
+export const glyphc=(o,w)=>new o.Glyph({name:"c",unicode:99,advanceWidth:w,path:(p=>{
+  p.moveTo(450,135).curveTo(420,50,370,-15,275,-15).curveTo(155,-15,60,90,60,245).curveTo(60,405,145,506,270,506)
+   .curveTo(380,506,420,430,450,365).lineTo(380,345).curveTo(350,400,315,430,270,430).curveTo(185,430,135,360,135,245)
+   .curveTo(135,145,180,60,270,60).curveTo(325,60,355,80,375,125).close(); return p;
+})(new o.Path())});
