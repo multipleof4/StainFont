532392d8a20cc277da6d83167b7e17c0a1782f4f
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:09 -0800

    Refactor: Spurred G, Double-Story g

--- src/glyphs/G.js
+++ src/glyphs/G.js
@@ -1,16 +1,12 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphG=(o,w)=>mk(o,"G",71,p=>{
-  p.moveTo(540,200);p.lineTo(460,180);p.curveTo(440,80,300,70,300,70);p.curveTo(160,70,160,630,300,630);
-  p.curveTo(440,630,460,520,460,520);p.lineTo(540,500);p.curveTo(520,710,300,710,300,710);
-  p.curveTo(60,710,60,-10,300,-10);p.curveTo(500,-10,540,150,540,150);p.lineTo(540,300);
-  p.lineTo(300,300);p.lineTo(300,220);p.lineTo(460,220);p.lineTo(460,200);p.close();
-  return p;
-},w);
-export const glyphg=(o,w)=>mk(o,"g",103,p=>{
-  p.moveTo(440,480);p.lineTo(440,0);p.curveTo(440,-150,300,-150,300,-150);p.curveTo(160,-150,160,0,160,0);
-  p.lineTo(240,0);p.curveTo(240,-80,360,-80,360,0);p.lineTo(360,60);p.curveTo(360,60,80,60,80,270);
-  p.curveTo(80,480,440,480,440,480);p.lineTo(520,480);p.lineTo(520,0);p.curveTo(520,-200,300,-200,300,-200);
-  p.curveTo(80,-200,80,0,80,0);p.lineTo(160,0);p.close();
-  p.moveTo(360,140);p.curveTo(360,400,160,400,160,270);p.curveTo(160,140,360,140,360,140);p.close();
-  return p;
-},w);
+export const glyphG=(o,w)=>new o.Glyph({name:"G",unicode:71,advanceWidth:w,path:(p=>{
+  p.moveTo(590,440).lineTo(590,240).lineTo(350,240).lineTo(350,315).lineTo(510,315).lineTo(510,345)
+   .curveTo(490,530,425,645,320,645).curveTo(205,645,145,500,145,350).curveTo(145,185,215,65,330,65).curveTo(420,65,480,105,510,185)
+   .lineTo(575,160).curveTo(535,45,440,-15,330,-15).curveTo(150,-15,60,165,60,350).curveTo(60,560,165,715,320,715).curveTo(440,715,550,610,590,440).close(); return p;
+})(new o.Path())});
+export const glyphg=(o,w)=>new o.Glyph({name:"g",unicode:103,advanceWidth:w,path:(p=>{
+  p.moveTo(475,230).lineTo(475,-45).curveTo(475,-185,405,-215,325,-215).lineTo(165,-215).lineTo(165,-150).lineTo(295,-150)
+  .curveTo(380,-150,395,-110,395,-40).lineTo(395,45).curveTo(355,0,300,-15,245,-15).curveTo(120,-15,45,85,45,250)
+  .curveTo(45,410,120,506,240,506).lineTo(485,506).lineTo(485,435).lineTo(280,435).curveTo(215,435,130,400,130,260)
+  .curveTo(130,135,185,65,265,65).curveTo(340,65,395,100,395,200).curveTo(395,290,350,320,280,335).lineTo(475,345).close()
+  .moveTo(415,395).curveTo(415,375,350,375,320,375).curveTo(365,395,400,415,415,445).close(); return p;
+})(new o.Path())});
