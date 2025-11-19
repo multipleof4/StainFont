b40c31a5c6f6acbd4d1242a8ffe1005d3e5c85c3
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:06:33 -0800

    Refactor H/h: Standardized stems and bars

--- src/glyphs/H.js
+++ src/glyphs/H.js
@@ -1,55 +1,13 @@
-const makeGlyph = (opentype, name, unicode, cmds, adv) =>
-  new opentype.Glyph({
-    name,
-    unicode,
-    advanceWidth: adv,
-    path: cmds(new opentype.Path())
-  });
-
-export const glyphH = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "H",
-    "H".codePointAt(0),
-    (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(100, 700);
-      p.lineTo(160, 700);
-      p.lineTo(160, 380);
-      p.lineTo(440, 380);
-      p.lineTo(440, 700);
-      p.lineTo(500, 700);
-      p.lineTo(500, 0);
-      p.lineTo(440, 0);
-      p.lineTo(440, 320);
-      p.lineTo(160, 320);
-      p.lineTo(160, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
-
-export const glyphh = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "h",
-    "h".codePointAt(0),
-    (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(100, 720);
-      p.lineTo(160, 720);
-      p.lineTo(160, 440);
-      p.curveTo(200, 470, 250, 490, 310, 490);
-      p.curveTo(420, 490, 480, 430, 480, 320);
-      p.lineTo(480, 0);
-      p.lineTo(420, 0);
-      p.lineTo(420, 320);
-      p.curveTo(420, 400, 380, 430, 310, 430);
-      p.curveTo(230, 430, 160, 390, 160, 300);
-      p.lineTo(160, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
+const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
+export const glyphH=(o,w)=>mk(o,"H",72,p=>{
+  p.moveTo(80,0);p.lineTo(80,700);p.lineTo(160,700);p.lineTo(160,390);p.lineTo(440,390);
+  p.lineTo(440,700);p.lineTo(520,700);p.lineTo(520,0);p.lineTo(440,0);p.lineTo(440,310);
+  p.lineTo(160,310);p.lineTo(160,0);p.close();
+  return p;
+},w);
+export const glyphh=(o,w)=>mk(o,"h",104,p=>{
+  p.moveTo(80,0);p.lineTo(80,710);p.lineTo(160,710);p.lineTo(160,300);p.curveTo(160,480,440,480,440,300);
+  p.lineTo(440,0);p.lineTo(360,0);p.lineTo(360,300);p.curveTo(360,400,240,400,240,300);
+  p.lineTo(240,0);p.close();
+  return p;
+},w);
