4ca02a86c75a0537c8cef52596515e3cc5a6d9c1
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:41 -0800

    Refactor: Double-V W/w

--- src/glyphs/W.js
+++ src/glyphs/W.js
@@ -1,13 +1,6 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphW=(o,w)=>mk(o,"W",87,p=>{
-  p.moveTo(20,700);p.lineTo(110,700);p.lineTo(220,200);p.lineTo(330,700);p.lineTo(430,700);
-  p.lineTo(540,200);p.lineTo(650,700);p.lineTo(740,700);p.lineTo(580,0);p.lineTo(490,0);
-  p.lineTo(380,400);p.lineTo(270,0);p.lineTo(180,0);p.close();
-  return p;
-},760);
-export const glyphw=(o,w)=>mk(o,"w",119,p=>{
-  p.moveTo(20,480);p.lineTo(100,480);p.lineTo(190,140);p.lineTo(280,480);p.lineTo(380,480);
-  p.lineTo(470,140);p.lineTo(560,480);p.lineTo(640,480);p.lineTo(510,0);p.lineTo(430,0);
-  p.lineTo(330,300);p.lineTo(230,0);p.lineTo(150,0);p.close();
-  return p;
-},660);
+export const glyphW=(o,w)=>new o.Glyph({name:"W",unicode:87,advanceWidth:w,path:(p=>{
+  p.moveTo(20,700).lineTo(100,700).lineTo(230,140).lineTo(370,700).lineTo(445,700).lineTo(580,140).lineTo(710,700).lineTo(790,700).lineTo(625,0).lineTo(545,0).lineTo(410,500).lineTo(275,0).lineTo(195,0).close(); return p;
+})(new o.Path())});
+export const glyphw=(o,w)=>new o.Glyph({name:"w",unicode:119,advanceWidth:w,path:(p=>{
+  p.moveTo(15,500).lineTo(95,500).lineTo(200,145).lineTo(305,500).lineTo(385,500).lineTo(495,145).lineTo(600,500).lineTo(680,500).lineTo(535,0).lineTo(465,0).lineTo(350,370).lineTo(235,0).lineTo(165,0).close(); return p;
+})(new o.Path())});
