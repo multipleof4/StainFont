66ee98248117b3e5374b0aac3f0fd0e70cbe8989
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:30 -0800

    Refactor: Tail Q, std q

--- src/glyphs/Q.js
+++ src/glyphs/Q.js
@@ -1,13 +1,9 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphQ=(o,w)=>mk(o,"Q",81,p=>{
-  p.moveTo(300,710);p.curveTo(60,710,60,-10,300,-10);p.curveTo(540,-10,540,710,300,710);p.close();
-  p.moveTo(300,630);p.curveTo(460,630,460,70,300,70);p.curveTo(140,70,140,630,300,630);p.close();
-  p.moveTo(320,140);p.lineTo(480,-80);p.lineTo(400,-80);p.lineTo(260,100);p.close();
-  return p;
-},w);
-export const glyphq=(o,w)=>mk(o,"q",113,p=>{
-  p.moveTo(440,-200);p.lineTo(440,480);p.lineTo(200,480);p.curveTo(-20,480,-20,0,200,0);
-  p.lineTo(360,0);p.lineTo(360,-200);p.close();
-  p.moveTo(360,400);p.lineTo(200,400);p.curveTo(60,400,60,80,200,80);p.lineTo(360,80);p.close();
-  return p;
-},w);
+export const glyphQ=(o,w)=>new o.Glyph({name:"Q",unicode:81,advanceWidth:w,path:(p=>{
+  p.moveTo(340,715).curveTo(530,715,620,540,620,360).curveTo(620,180,525,50,370,10).lineTo(460,-90).lineTo(385,-120).lineTo(285,10).curveTo(155,30,65,170,65,350).curveTo(65,555,170,715,340,715).close()
+   .moveTo(340,70).curveTo(375,70,405,95,430,130).lineTo(305,270).lineTo(280,245).lineTo(345,70).curveTo(343,70,341,70,340,70).close() // tail bisect
+   .moveTo(530,360).curveTo(530,420,515,635,340,635).curveTo(220,635,155,510,155,350).curveTo(155,190,220,70,285,70).lineTo(355,210).lineTo(385,185).lineTo(300,85).curveTo(470,85,530,180,530,360).close(); return p;
+})(new o.Path())});
+export const glyphq=(o,w)=>new o.Glyph({name:"q",unicode:113,advanceWidth:w,path:(p=>{
+  p.moveTo(460,-200).lineTo(460,500).lineTo(385,500).lineTo(385,430).curveTo(340,495,260,506,215,506).curveTo(95,506,45,380,45,255).curveTo(45,90,120,-15,215,-15).curveTo(300,-15,340,50,385,90).lineTo(385,-200).close()
+  .moveTo(250,60).curveTo(180,60,130,110,130,250).curveTo(130,385,180,435,260,435).curveTo(340,435,385,350,385,255).curveTo(385,120,325,60,250,60).close(); return p;
+})(new o.Path())});
