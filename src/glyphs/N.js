37ac506b90595d3f86a1ea6f393f1aa74b432ec4
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:06:46 -0800

    Refactor N/n: Diagonals and arches

--- src/glyphs/N.js
+++ src/glyphs/N.js
@@ -1,53 +1,12 @@
-const makeGlyph = (opentype, name, unicode, cmds, adv) =>
-  new opentype.Glyph({
-    name,
-    unicode,
-    advanceWidth: adv,
-    path: cmds(new opentype.Path())
-  });
-
-export const glyphN = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "N",
-    "N".codePointAt(0),
-    (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(100, 700);
-      p.lineTo(160, 700);
-      p.lineTo(440, 120);
-      p.lineTo(440, 700);
-      p.lineTo(500, 700);
-      p.lineTo(500, 0);
-      p.lineTo(440, 0);
-      p.lineTo(160, 580);
-      p.lineTo(160, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
-
-export const glyphn = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "n",
-    "n".codePointAt(0),
-    (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(100, 480);
-      p.lineTo(160, 480);
-      p.lineTo(160, 440);
-      p.curveTo(200, 470, 250, 490, 310, 490);
-      p.curveTo(420, 490, 480, 430, 480, 320);
-      p.lineTo(480, 0);
-      p.lineTo(420, 0);
-      p.lineTo(420, 320);
-      p.curveTo(420, 400, 380, 430, 310, 430);
-      p.curveTo(230, 430, 160, 390, 160, 300);
-      p.lineTo(160, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
+const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
+export const glyphN=(o,w)=>mk(o,"N",78,p=>{
+  p.moveTo(80,0);p.lineTo(80,700);p.lineTo(160,700);p.lineTo(440,180);p.lineTo(440,700);
+  p.lineTo(520,700);p.lineTo(520,0);p.lineTo(440,0);p.lineTo(160,520);p.lineTo(160,0);p.close();
+  return p;
+},w);
+export const glyphn=(o,w)=>mk(o,"n",110,p=>{
+  p.moveTo(80,0);p.lineTo(80,480);p.lineTo(160,480);p.lineTo(160,300);p.curveTo(160,480,440,480,440,300);
+  p.lineTo(440,0);p.lineTo(360,0);p.lineTo(360,300);p.curveTo(360,400,240,400,240,300);
+  p.lineTo(240,0);p.close();
+  return p;
+},w);
